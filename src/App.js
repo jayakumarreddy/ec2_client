d0ea9b938a13f22b4408d5bcb6988937430f1737
Jaya Kumar Reddy Regalla <rjayakumarreddy887@gmail.com>
2018-12-21 20:53:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React version 2
+            Learn React version 2 only restart
           </a>
         </header>
       </div>
